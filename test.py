74979d475e90dc44d8eaade0fa34a892561117d0
Christos8 <chris02hitas@gmail.com>
2024-12-30 17:00:17 +0200

    create tables

--- test.py
+++ test.py
@@ -1,3 +1,29 @@
 import sqlite3
 
 conn = sqlite3.connect('test.db')
+
+c = conn.cursor()
+
+#c.execute("""CREATE TABLE customers (
+  #        first_name text,
+   #       last_nane text, 
+   #       email text
+   #       )""")
+
+#many_customer = [('Wes', 'Brown', 'email123'),
+  #               ('Stepth', 'Kuewa', 'few123'),
+  #               ('Giorgis', 'Vlakas', 'meptyxio'),
+   #              ]
+
+#c.executemany("INSERT INTO customers VALUES (?,?,?)", many_customer)
+
+c.execute("SELECT * FROM customers")
+
+items = c.fetchall()
+  
+for item in items:
+    print(item[0])
+
+conn.commit()
+
+conn.close()
