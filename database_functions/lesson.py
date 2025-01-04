2e0e0f7fae7dd40284296169131f4ee301852fec
Christos8 <chris02hitas@gmail.com>
2025-01-04 17:29:03 +0200

    nothing really

--- database_functions/lesson.py
+++ database_functions/lesson.py
@@ -1,6 +1,7 @@
 from entity_instances.lesson_in import LessonIn
 from create_tables import CREATE_LESSON_TABLE
 from insert_tables import INSERT_LESSON
+from select_tables import SELECT_ALL_LESSONS
 
 
 class Lesson:
@@ -13,4 +14,8 @@ class Lesson:
         with self.connection:
             self.cursor.execute(INSERT_LESSON, (lesson.id, lesson.date, lesson.startTime, lesson.endTime, lesson.difficulty, lesson.coachID))
 
-    
+    def return_lesson(self):
+        self.cursor.execute(SELECT_ALL_LESSONS)
+        _, *lesson_data = self.cursor.fetchall()
+        lesson = LessonIn(*lesson_data)
+        return lesson
