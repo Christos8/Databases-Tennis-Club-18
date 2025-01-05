236b6782e8cc5f05988e7ee593a519011d45e1e3
Christos8 <chris02hitas@gmail.com>
2025-01-05 21:06:46 +0200

    n

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from datetime import date
 
 startdate = '25-1-21'
-date = startdate.strftime("%d/%m/%Y")
+#date = startdate.strftime("%d/%m/%Y")
 
-print(date)
+if '25/01/2025' > '26/01/2025':
+    print("True")
+else: 
+    print("False")
